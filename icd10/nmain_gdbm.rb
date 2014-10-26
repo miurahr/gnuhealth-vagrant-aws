9d04303e8f9bbc4c36face461b48969a590e837a
Hiroshi Miura <miurahr@linux.com>
2014-10-26 17:03:26 +0900

    only create db only with no icd detail code
    Signed-off-by: Hiroshi Miura <miurahr@linux.com>

--- icd10/nmain_gdbm.rb
+++ icd10/nmain_gdbm.rb
@@ -3,6 +3,8 @@ require 'gdbm'
 
 gdbm = GDBM.new("nmain314.db")
 CSV.open("nmain314.csv", 'r') do |row|
-  gdbm[row[6]] = row[2]
+  if row[7] == ""
+    gdbm[row[6]] = row[2]
+  end
 end
 gdbm.close
